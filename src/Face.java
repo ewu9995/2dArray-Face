9b60e76d112de6b7eddd9653af3b5bcde1c9caf5
Author <PC@Dell>
2017-12-04 15:56:31 -0500

    Commit

--- src/Face.java
+++ src/Face.java
@@ -3,10 +3,9 @@ public class Face {
 
 	
 	public static void main(String[] args) {
-	int arr [][];
 	
-	arr = new int [5][5];
-	arr [1][1] = 0;
+	int[][] arr = new int [5][5];
+	arr[1][1]=0;
 	arr[1][3]=0;
 	arr[2][2]=4;
 	arr[3][0]=1;
@@ -14,26 +13,30 @@ public class Face {
 	arr[4][2]=1;
 	arr[4][3]=1;
 	arr[3][4]=1;
-	
-	
-	System.out.println(arr);
-	
-	}
-	
-	
-	
-	
-	
-	
-	public static void print2d(int[][] arr) {
-		for (int i =0; i < arr.length; i++) {
-			for (int j =0; j< arr[i].length; j++){
-				System.out.println(arr);
-			}
-			System.out.println();
+
+	for (int i =0; i < arr.length; i++) {
+		for (int j =0; j< arr[i].length; j++){
+			System.out.println(arr);
 		}
+		System.out.println();
 	}
 	
-	
-	
+
+	  for (int[] a : arr) {
+	      for (int i : a) {
+	          System.out.print(i + "\t");
+	      }
+	      System.out.println("\n");
+	  	}
+	}
 }
+
+	
+	
+	
+	
+	
+	
+	
+
+
