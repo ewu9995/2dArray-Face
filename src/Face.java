895103c7fe227c64ac15e81e8627e2d5a057f96a
Author <PC@Dell>
2017-12-04 15:37:41 -0500

    Commit

--- src/Face.java
+++ src/Face.java
@@ -1,16 +1,24 @@
 
 public class Face {
 
-	int [][] Arr = new int [5][5];
-	Arr [1][1] = 0;
-	Arr[1][3]=0;
-	Arr[2][2]=4;
-	Arr[3][0]=1;
-	Arr[4][1]=1;
-	Arr[4][2]=1;
-	Arr[4][3]=1;
-	Arr[3][4]=1;
 	
+	public static void main(String[] args) {
+	int arr [][];
+	
+	arr = new int [5][5];
+	arr [1][1] = 0;
+	arr[1][3]=0;
+	arr[2][2]=4;
+	arr[3][0]=1;
+	arr[4][1]=1;
+	arr[4][2]=1;
+	arr[4][3]=1;
+	arr[3][4]=1;
+	
+	
+	System.out.println(arr);
+	
+	}
 	
 	
 	
@@ -25,11 +33,6 @@ public class Face {
 			System.out.println();
 		}
 	}
-	
-	
-	
-	
-	
 	
 	
 	
